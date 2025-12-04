058254c93f95b38e87b184e3b9be3e9ea958cd73
pedroaf1 <159036696+Pedro1AF@users.noreply.github.com>
2025-12-04 13:40:11 -0300

    Delete producaoCRUD directory
    substituindo por novo codigo aida a ser implementado  no firebase

--- producaoCRUD/static/diretorio.js
+++
@@ -1,9 +0,0 @@
-// Abrir modal
-document.getElementById("openModalBtn").onclick = function () {
-    document.getElementById("modalBg").style.display = "flex";
-};
-
-// Fechar modal
-function closeModal() {
-    document.getElementById("modalBg").style.display = "none";
-}
